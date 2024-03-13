477256ddad2032a4e7924138e55ce29700a47877
Valkhnut <dhjob@yandex.by>
2024-03-13 20:51:12 +0300

    Update default User model

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+urlpatterns = [
+    #  pre login url
+    #  path('login/', views.user_login, name='login'),
+
+
+    # login and logout urls
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #
+    # # urls for changing password
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #
+    # # urls for reset password
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+]
