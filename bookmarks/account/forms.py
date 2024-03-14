1aa60d990941f64b655e485901cca134dcf04e17
Valkhnut <dhjob@yandex.by>
2024-03-14 21:28:27 +0300

    update forms

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -30,6 +30,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Password don't match")
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use!')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
@@ -41,3 +47,10 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['date_of_birth', 'photo']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use!')
+        return data
